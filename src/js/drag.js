5ad8b8ce0a7a7bbe20aac947c1d164af6902c185
sutootototo2002 <sutootototo2002@163.com>
2018-01-10 20:13:29 +0800

    change2 drag.js

--- src/js/drag.js
+++ src/js/drag.js
@@ -1,3 +1,7 @@
-function drag () {
-    console.log("drag");
+function drag (id) {
+    //console.log("drag");
+    var obj = document.getElementById(id);
+    obj.onmousedown = function(){
+        //鼠标按下时会怎么样
+    }
 }
