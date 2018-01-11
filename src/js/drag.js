13a3b62833949e3fd4b6bb2a1f401bc262ccbc50
sutootototo2002 <sutootototo2002@163.com>
2018-01-11 17:53:14 +0800

    change drag.js new2

--- src/js/drag.js
+++ src/js/drag.js
@@ -3,7 +3,9 @@ function drag (id) {
     var disx = 0;
     var disy = 0;//15点56分
     var obj = document.getElementById(id);
-    obj.onmousedown = function(){
+    obj.onmousedown = function(e){
         //鼠标按下时会怎么样
+        disx = e.pageX - obj.offsetLeft;
+        disy = e.pageY - obj.offsetTop;
     }
 }
