c382a0fd4431f35faf361ac6c0a856c56cf2dbc5
sutootototo2002 <sutootototo2002@163.com>
2018-01-11 16:06:47 +0800

    change4 drag.js

--- src/js/drag.js
+++ src/js/drag.js
@@ -1,7 +1,7 @@
 function drag (id) {
     //console.log("drag");
     var disx = 0;
-    var disy = 0;
+    var disy = 0;//15点56分
     var obj = document.getElementById(id);
     obj.onmousedown = function(){
         //鼠标按下时会怎么样
