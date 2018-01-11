1e6abd84c724754cb7bec7b23c61d2f38a062125
sutootototo2002 <sutootototo2002@163.com>
2018-01-11 16:39:00 +0800

    flowryan1 changed1

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <div id='div1'></div>
     <router-view/>
   </div>
 </template>
@@ -12,6 +12,12 @@ export default {
 </script>
 
 <style>
+#div1{
+  width:100px;
+  height: 100px;
+  border:1px solid red;
+  
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
