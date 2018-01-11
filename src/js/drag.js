cbe1f0efc684e8ed124628275a56ab3fbed6b660
sutootototo2002 <sutootototo2002@163.com>
2018-01-11 15:08:28 +0800

    change3 drag.js

--- src/js/drag.js
+++ src/js/drag.js
@@ -1,5 +1,7 @@
 function drag (id) {
     //console.log("drag");
+    var disx = 0;
+    var disy = 0;
     var obj = document.getElementById(id);
     obj.onmousedown = function(){
         //鼠标按下时会怎么样
